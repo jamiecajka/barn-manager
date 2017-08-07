d5f27d9eff2127d2805d34ec8bf43d9047ab1167
Jamie <Jamie@Jamies-MacBook-Pro.local>
2017-08-07 15:28:10 -0400

    fixed tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Event do
-  it { should have_valid(:date).when('7/31/17') }
+  it { should have_valid(:date).when('2017/09/01') }
   it { should have_valid(:description).when('description') }
   it { should have_valid(:horse_id).when(1, 20, 300) }
 
@@ -11,10 +11,10 @@ end
 
 RSpec.describe Event do
   context 'making a new charge' do
-    let(:event) {Event.new(date: '7/31/17', description: 'description', horse_id: 1)}
+    let(:event) {Event.new(date: Date.current, description: 'description', horse_id: 1)}
 
     it 'has a date' do
-      expect(event.date).to include('7/31/17')
+      expect(event.date).to eq(Date.current)
     end
 
     it 'has a description' do
