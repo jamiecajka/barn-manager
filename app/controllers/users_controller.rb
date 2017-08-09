19c4bca27ee392fc6220a734da525936823f5aea
Jamie <Jamie@Jamies-MacBook-Pro.local>
2017-08-09 10:54:52 -0400

    changed navbar and background

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = 'Account was successfully created'
-      redirect_to @user
+      redirect_to current_user_path
     else
       render :new
     end
